ad80222c460dc369f37afeaf8596aec64b3dafb8
kristiankaspersen <kristiankasp@gmail.com>
2025-06-14 21:43:24 +0200

    This is the version used when I handed in the thesis.

--- schedsimulator/enqueue_task.py
+++ schedsimulator/enqueue_task.py
@@ -1,15 +1,18 @@
 import time
 
+from schedsimulator.enums.process_status import TaskStatus
 from schedsimulator.enums.task_type import TaskType
-from schedsimulator.globals import sysctl_sched_base_slice, PLACE_LAG, INIT_TREE, TICK_NSEC
+from schedsimulator.globals import sysctl_sched_base_slice, PLACE_LAG, INIT_TREE, TICK_NSEC, INTERACTIVE_TIME_WINDOW
 #from schedsimulator.structures.task import Task
 from schedsimulator.utils import avg_vruntime, calc_delta_fair, avg_vruntime_add
 from schedsimulator.update_current import update_curr
 import schedsimulator.globals as globals
 
+# These should have no effect, they are put in globals.py.
 initial_deadline_diff = 0
 initial_vr_diff = 0
-
+io_negative_count = 0
+interactive_negative_count = 0
 
 def enqueue_entity(cfs_rq, task):
 
@@ -40,16 +43,18 @@ def enqueue_entity(cfs_rq, task):
     """
     curr = cfs_rq.curr == task
 
-    # Need to update how many tasks there are of the types:
-    if task.type == TaskType.RESP:
-        cfs_rq.num_interactive += 1
-    elif task.type == TaskType.CPU:
-        cfs_rq.num_cpu += 1
-    else:
-        cfs_rq.num_other += 1
+    #FOR REPLICATING SCENARIO 2, add this
+    #Need to update how many tasks there are of the types:
+    # This is the bug I found that have an effect on scenario 2 in particular, managed to count the task two times.
+    # Here, and in add_task_type() function. I think i moved this in to a function, and just forgot to remove it. The same for dequeue.
+    # if task.type == TaskType.RESP:
+    #     cfs_rq.num_interactive += 1
+    # elif task.type == TaskType.CPU:
+    #     cfs_rq.num_cpu += 1
+    # else:
+    #     cfs_rq.num_other += 1
 
     if curr:
-        print("IS curr the one being enqueued?")
         place_entity(cfs_rq, task)
 
     resched = update_curr(cfs_rq)
@@ -60,29 +65,21 @@ def enqueue_entity(cfs_rq, task):
     if INIT_TREE:
         globals.initial_vr_diff += TICK_NSEC
         task.vruntime = globals.initial_vr_diff
-        #initial_vr_diff += TICK_NSEC
-        #globals.initial_deadline_diff += 1
         vslice = calc_delta_fair(task.slice, task)
         task.deadline = task.vruntime + vslice
 
-    # account_entity_enqueue() TODO: Add this later
     cfs_rq.nr_queued += 1 # Only do this for now. Its done in account_entity_enqueue()
     assert cfs_rq.nr_queued >= 0, f"BUG: nr_queued went negative at PID={task.pid}"
 
-    # update_stats_enqueue_fair() TODO: perhaphs necessary for RL
     if not curr:
         assert task is not cfs_rq.curr, f"BUG: tried to enqueue curr (pid={task.pid}) into tree"
         __enqueue_entity(cfs_rq, task)
 
     task.on_rq = True
-    assert task.on_rq, "dsadnkjadnka"
-    print(task.pid)
 
     return resched
 
-
 def place_entity(cfs_rq, task):
-
     if not task.custom_slice:
         task.slice = sysctl_sched_base_slice
 
@@ -92,10 +89,10 @@ def place_entity(cfs_rq, task):
         elif task.type == TaskType.RESP:
             task.slice += globals.interactive_offset
 
+
     vruntime = avg_vruntime(cfs_rq)
     vslice = calc_delta_fair(task.slice, task)
     lag = 0
-    # TODO: Implement a lag version as well, here, where I try one with or without the lag implementation.
     if PLACE_LAG and cfs_rq.nr_queued > 0 and task.vlag != 0:
         """
         Make sure lag correction only applies when: 
@@ -120,11 +117,17 @@ def place_entity(cfs_rq, task):
         if load == 0:
             load = 1
 
+        # Statistics added later. For counting negative lags.
+        if task.type == TaskType.IO:
+            if lag < 0:
+                globals.io_negative_count += 1
+        elif task.type == TaskType.RESP:
+            if lag < 0:
+                globals.interactive_negative_count += 1
 
         # Apply lag inflation to preserve the original lag after placement
         corrected_lag = ((load + task.weight) * lag)/ load
         lag = corrected_lag
-
 
     # Place the task into the timeline (shift back by lag if needed)
     task.vruntime = vruntime - lag
@@ -134,8 +137,9 @@ def place_entity(cfs_rq, task):
 
 
 def __enqueue_entity(cfs_rq, task):
-    #TODO aslo need to insert the min_vruntime part
-    # Think we are good here, with the min_vruntime..
+    assert task.status != TaskStatus.BLOCKED, f"BUG: Tried to enqueue BLOCKED task (pid={task.pid})"
+    assert task.status != TaskStatus.EXIT, f"BUG: Tried to enqueue EXIT task (pid={task.pid})"
+
     task.enqueue_time = time.time_ns()
     task.enqueue_time_virtual = cfs_rq.virtual_global_clock_ns
     avg_vruntime_add(cfs_rq, task)
@@ -143,24 +147,22 @@ def __enqueue_entity(cfs_rq, task):
     add_task_type(cfs_rq, task)
 
 
+
 def add_task_type(cfs_rq, task):
     if task.type == TaskType.CPU:
         cfs_rq.num_cpu += 1
     elif task.type == TaskType.RESP:
         cfs_rq.num_interactive += 1
+        log_interactive_arrival(cfs_rq, cfs_rq.virtual_global_clock_ns, INTERACTIVE_TIME_WINDOW)
     else:
         cfs_rq.num_other += 1
 
+def log_interactive_arrival(cfs_rq, now, window=5):
+    cfs_rq.interactive_arrivals.append(now)
 
-
-
-
-
-
-
-
-
-
+    # Remove anything outside the recent window
+    while cfs_rq.interactive_arrivals and cfs_rq.interactive_arrivals[0] < now - window:
+        cfs_rq.interactive_arrivals.popleft()
 
 
 def enqueue_task_fair(task):
